ecfe29a6f3ce1104692231da6f8da359782b0e85
Cornelia-Lucaci <cornelia.lucaci@student.usv.ro>
2026-04-27 14:37:14 +0300

    Update

--- InchirieriAuto.WPF/MainWindow.xaml.cs
+++ InchirieriAuto.WPF/MainWindow.xaml.cs
@@ -1,33 +1,119 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Windows;
-using InchirieriAuto; // IMPORTANT
+using System.Windows.Media;
+using InchirieriAuto;
 
 namespace WpfInchirieri
 {
     public partial class MainWindow : Window
     {
-        private List<Masina> masini;
+        private List<Masina> masini = new List<Masina>();
+
+        // constante (CERINȚĂ)
+        private const int AN_MIN = 1900;
+        private const int AN_MAX = 2026;
 
         public MainWindow()
         {
             InitializeComponent();
-            IncarcaMasini();
+
+            cbCuloare.ItemsSource = Enum.GetValues(typeof(Culoare));
         }
 
-        private void IncarcaMasini()
+        private void AdaugaMasina_Click(object sender, RoutedEventArgs e)
         {
-            masini = new List<Masina>()
+            ResetCulori();
+
+            bool valid = true;
+
+            string marca = txtMarca.Text;
+            string model = txtModel.Text;
+
+            int an;
+
+            //  VALIDARE MARCA
+            if (string.IsNullOrWhiteSpace(marca))
             {
-                new Masina("BMW", "X5", 2020, Culoare.Alb, Optiuni.Navigatie | Optiuni.SenzoriParcare),
-                new Masina("Audi", "A4", 2019, Culoare.Roz, Optiuni.TrapaPanoramica),
-                new Masina("Dacia", "Logan", 2018, Culoare.Albastru, Optiuni.Nimic)
-            };
+                lblMarca.Foreground = Brushes.Red;
+                valid = false;
+            }
 
+            // VALIDARE MODEL
+            if (string.IsNullOrWhiteSpace(model))
+            {
+                lblModel.Foreground = Brushes.Red;
+                valid = false;
+            }
+
+            // VALIDARE AN
+            if (!int.TryParse(txtAn.Text, out an) || an < AN_MIN || an > AN_MAX)
+            {
+                lblAn.Foreground = Brushes.Red;
+                valid = false;
+            }
+
+            // VALIDARE CULOARE
+            if (cbCuloare.SelectedItem == null)
+            {
+                MessageBox.Show("Selectati culoarea!");
+                valid = false;
+            }
+
+            if (!valid)
+            {
+                MessageBox.Show("Date invalide!");
+                return;
+            }
+
+            // optiuni
+            Optiuni opt = Optiuni.Nimic;
+
+            if (chkNav.IsChecked == true)
+                opt |= Optiuni.Navigatie;
+
+            if (chkTrapa.IsChecked == true)
+                opt |= Optiuni.TrapaPanoramica;
+
+            if (chkSenzori.IsChecked == true)
+                opt |= Optiuni.SenzoriParcare;
+
+            // creare masina
+            Masina m = new Masina(
+                marca,
+                model,
+                an,
+                (Culoare)cbCuloare.SelectedItem,
+                opt
+            );
+
+            masini.Add(m);
+
+            dgMasini.ItemsSource = null;
             dgMasini.ItemsSource = masini;
+
+            MessageBox.Show("Masina adaugata!");
+
+            ClearForm();
         }
-        private void Reload_Click(object sender, RoutedEventArgs e)
+
+        private void ResetCulori()
         {
-            IncarcaMasini();
+            lblMarca.Foreground = Brushes.Black;
+            lblModel.Foreground = Brushes.Black;
+            lblAn.Foreground = Brushes.Black;
+        }
+
+        private void ClearForm()
+        {
+            txtMarca.Text = "";
+            txtModel.Text = "";
+            txtAn.Text = "";
+            cbCuloare.SelectedItem = null;
+
+            chkNav.IsChecked = false;
+            chkTrapa.IsChecked = false;
+            chkSenzori.IsChecked = false;
         }
     }
 }
