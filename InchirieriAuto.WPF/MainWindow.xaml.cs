052f513e31d53024a1455b7561aafa1ae642ac8e
Cornelia-Lucaci <cornelia.lucaci@student.usv.ro>
2026-04-27 13:49:01 +0300

    Update.Tema6

---
+++ InchirieriAuto.WPF/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using InchirieriAuto; // IMPORTANT
+
+namespace WpfInchirieri
+{
+    public partial class MainWindow : Window
+    {
+        private List<Masina> masini;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            IncarcaMasini();
+        }
+
+        private void IncarcaMasini()
+        {
+            masini = new List<Masina>()
+            {
+                new Masina("BMW", "X5", 2020, Culoare.Alb, Optiuni.Navigatie | Optiuni.SenzoriParcare),
+                new Masina("Audi", "A4", 2019, Culoare.Roz, Optiuni.TrapaPanoramica),
+                new Masina("Dacia", "Logan", 2018, Culoare.Albastru, Optiuni.Nimic)
+            };
+
+            dgMasini.ItemsSource = masini;
+        }
+        private void Reload_Click(object sender, RoutedEventArgs e)
+        {
+            IncarcaMasini();
+        }
+    }
+}
--- Proiect/Program.cs
+++ Proiect/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using InchirieriAuto;
 
+
 namespace InchirieriAuto
 {
     class Program
